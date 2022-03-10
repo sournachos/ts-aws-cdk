0370dbcd87c1e0fb556776bbe2c4316e97579e27
Jose Rivera <80435061+sournachos@users.noreply.github.com>
2022-03-10 16:43:11 -0500

    added s3 bucket (auto-managed encryption) and cloudformation export

--- typescript-cdk/lib/typescript-cdk-stack.ts
+++ typescript-cdk/lib/typescript-cdk-stack.ts
@@ -1,16 +1,19 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+import {aws_s3 as s3} from 'aws-cdk-lib';
 
 export class TypescriptCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "DocumentsBucket", {
+      encryption: s3.BucketEncryption.S3_MANAGED
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TypescriptCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CfnOutput(this, "DocumentsBucketNameExport", {
+      value: bucket.bucketName,
+      exportName: "DocumentsBucketName"
+    });
+
   }
 }
